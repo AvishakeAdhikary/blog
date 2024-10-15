c5cdb2f6a2118d93dd30978599f98165cddf6757
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-10-16 01:08:14 +0530

    Updated UI

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -4,15 +4,16 @@ import {
     NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { ModeToggle } from "./modeToggle";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <NavigationMenu className="p-4 gap-2">
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    Blog
+        <NavigationMenu className="flex p-4 gap-2 max-w-full">
+            <NavigationMenuList className="flex justify-between max-w-full">
+                <NavigationMenuItem className="font-extrabold">
+                    <Link href="/">Blog</Link>
                 </NavigationMenuItem>
-                <NavigationMenuItem>
+                <NavigationMenuItem className="flex flex-row">
                     <ModeToggle />
                 </NavigationMenuItem>
             </NavigationMenuList>
