b635008f6295d839648e1c6541e455d99f845d15
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-10-15 17:02:48 +0530

    Updated page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,31 +1,31 @@
-import { getPostBySlug } from "@/lib/api";
-import { author } from "@/lib/constants";
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
+// import { getPostBySlug } from "@/lib/api";
+// import { author } from "@/lib/constants";
+// import { Metadata } from "next";
+// import { notFound } from "next/navigation";
 
-type Params = {
-    params: {
-      slug: string;
-    };
-};
+// type Params = {
+//     params: {
+//       slug: string;
+//     };
+// };
 
-export function generateMetadata({ params }: Params): Metadata {
-    const post = getPostBySlug(params.slug);
+// export function generateMetadata({ params }: Params): Metadata {
+//     const post = getPostBySlug(params.slug);
   
-    if (!post) {
-      return notFound();
-    }
+//     if (!post) {
+//       return notFound();
+//     }
   
-    const title = `${post.title} | ${author}'s Blog`;
+//     const title = `${post.title} | ${author}'s Blog`;
   
-    return {
-      title,
-      openGraph: {
-        title,
-        images: [post.ogImage.url],
-      },
-    };
-}
+//     return {
+//       title,
+//       openGraph: {
+//         title,
+//         images: [post.ogImage.url],
+//       },
+//     };
+// }
 
 export default function Post() {
   return <div></div>
