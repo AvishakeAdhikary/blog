6de0f896d9e0a476ff66b56a0fc6dab8fe6b6f3c
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-11-19 13:02:29 +0530

    Changed to rehype

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,30 @@
-import { remark } from "remark";
-import html from "remark-html";
+import remarkParse from "remark-parse";
+import { rehype } from "rehype";
+import rehypePrism from "rehype-prism";
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify';
+import { unified } from "unified";
+
+// Manual Prism JS Module Load
+import "prismjs/prism.js";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js";
+import "prismjs/plugins/show-language/prism-show-language.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypePrism, { plugins: [
+      "line-numbers",
+      "copy-to-clipboard",
+      "show-language"
+    ] })
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
